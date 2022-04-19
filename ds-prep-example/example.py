8cf51a475e32fba6888a3c783d3d21707f5bf743
Daniel Shi <z.daniel.shi@gmail.com>
2022-04-19 05:06:32 +0000

    Uploading with the correct name of the pull request.

--- ds-prep-example/example.py
+++ ds-prep-example/example.py
@@ -8,7 +8,7 @@ print("I would like to analyze {} to learn/predict {}.\n".format(
 
   # write what you would like to analyze in the quotes here 👇
 
-  "lots of data for fun",
+  "lots of data for funsies",
 
   # write what you would like to learn from that analysis here 👇
 
