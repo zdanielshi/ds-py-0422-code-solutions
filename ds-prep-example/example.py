7a9465b1e363889cff6b78bca4680880c23f593c
Daniel Shi <z.daniel.shi@gmail.com>
2022-04-19 03:51:11 +0000

    Completed example exercise

---
+++ ds-prep-example/example.py
@@ -0,0 +1,17 @@
+print("""
+  ===============
+  ds-prep-example
+  ===============
+""")
+
+print("I would like to analyze {} to learn/predict {}.\n".format(
+
+  # write what you would like to analyze in the quotes here 👇
+
+  "lots of data",
+
+  # write what you would like to learn from that analysis here 👇
+
+  "how to make better decisions"
+
+))
